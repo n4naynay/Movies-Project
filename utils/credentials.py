287af846f60e1e7df9748afa94cacaba47e4010b
n4naynay <nenedurugo@yahoo.co.uk>
2023-10-15 21:52:57 +0100

    initial project updates

---
+++ utils/credentials.py
@@ -0,0 +1,7 @@
+server = "mysqlserver1414.database.windows.net"
+database = "mySampleDatabase"
+username = "azureuser"
+password = 'Nkem1414#'
+
+app_password = "wftm tyzt qpmf rqwk"
+
---
+++ utils/jira_info.py
@@ -0,0 +1,12 @@
+
+fields = {
+    "project": {"key": ""},
+    "issuetype": {"name": ""},
+    "summary": "",
+    "description": "",
+    'customfield_10040': "",
+    'customfield_10041': "",
+    'customfield_10034': "",
+    'customfield_10043': ""
+}
+
